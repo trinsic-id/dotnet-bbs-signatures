d2898b580ddfd35beff6198d5164faf1c9323828
Tomislav Markovski <tmarkovski@gmail.com>
2020-06-05 08:02:58 -0400

    Initial project with sample Bls calls (#1)

---
+++ src/BbsSignatures/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("BbsSignatures.Tests")]
+
+namespace BbsSignatures
+{
+    internal class Constants
+    {
+        internal const string BbsSignaturesLibrary = "ffi_bbs_signatures";
+    }
+}
