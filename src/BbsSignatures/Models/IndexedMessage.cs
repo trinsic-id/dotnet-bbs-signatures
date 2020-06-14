beb1eb3b85d8043a74d65bb13c49a6dc3b35f585
Tomislav Markovski <tmarkovski@gmail.com>
2020-06-14 10:26:33 -0400

    Refactor keys

--- src/BbsSignatures/Models/IndexedMessage.cs
+++ src/BbsSignatures/Models/IndexedMessage.cs
@@ -1,9 +1,18 @@
 ﻿namespace BbsSignatures
 {
+    /// <summary>
+    /// Represents a message and it's index within a collection
+    /// </summary>
     public struct IndexedMessage
     {
+        /// <summary>
+        /// The message
+        /// </summary>
         public string Message;
 
+        /// <summary>
+        /// The message index
+        /// </summary>
         public uint Index;
     }
 }
