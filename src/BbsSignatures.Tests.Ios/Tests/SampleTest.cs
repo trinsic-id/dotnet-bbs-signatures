91c2867c6af55109c03a82943f9d48a3b915a106
Tomislav Markovski <tomislav@streetcred.id>
2020-06-16 13:46:30 -0400

    Ios test

---
+++ src/BbsSignatures.Tests.Ios/Tests/SampleTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BbsSignatures.Tests.Ios.Tests
+{
+    public class SampleTest
+    {
+        [Test]
+        public void TestKey()
+        {
+            var key = BbsProvider.GenerateKey();
+
+            Assert.NotNull(key);
+        }
+    }
+}
--- src/BbsSignatures/Constants.cs
+++ src/BbsSignatures/Constants.cs
@@ -7,7 +7,7 @@ namespace BbsSignatures
     internal class Constants
     {
 #if __IOS__
-        internal const string BbsSignaturesLibrary = "*";
+        internal const string BbsSignaturesLibrary = "__Internal";
 #else
         internal const string BbsSignaturesLibrary = "bbs";
 #endif
