394abed9356225386bd6b09075fa44580a725ebf
Tomislav Markovski <tmarkovski@gmail.com>
2020-07-28 19:22:36 -0400

    VC data model

---
+++ src/W3C.DidCore/LinkedDataObject.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace W3C.DidCore
+{
+    public class LinkedDataObject : JObject
+    {
+        public LinkedDataObject()
+        {
+        }
+
+        public LinkedDataObject(JObject other) : base(other)
+        {
+        }
+
+        public string Id
+        {
+            get => this["id"]?.Value<string>();
+            set => this["id"] = value;
+        }
+
+        public string ObjectType
+        {
+            get => this["type"]?.Value<string>();
+            set => this["type"] = value;
+        }
+    }
+}
