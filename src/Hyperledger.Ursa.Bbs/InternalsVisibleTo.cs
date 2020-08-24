ba10f8ead7b50d93b9673126d53ad701f3e828e6
Tomislav Markovski <tmarkovski@gmail.com>
2020-08-24 08:56:52 -0400

    Remove ld proofs

--- src/Hyperledger.Ursa.Bbs/InternalsVisibleTo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Hyperledger.Ursa.Bbs.Tests")]
-[assembly: InternalsVisibleTo("Hyperledger.Ursa.Bbs.Tests.Ios")]
-[assembly: InternalsVisibleTo("Hyperledger.Ursa.Bbs.Tests.Android")]
